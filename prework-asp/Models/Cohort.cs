9193a8f4c3cd24402db85c16537e61fb1e20473d
DUSTIN FLEMING <dfleming14@cnm.edu>
2018-10-04 21:33:15 -0600

    Added Cohort, and ITeacher classes.

---
+++ prework-asp/Models/Cohort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Cohort
+    {
+        public string CohortName { get; set; }
+
+        public string Schedule { get; set; }
+
+        public bool InProgres { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int CohortID { get; set; }
+
+    }
+}
