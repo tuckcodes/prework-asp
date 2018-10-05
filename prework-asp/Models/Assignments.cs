3d5330e2d1700da7985b8651015a7109f2c948f0
DUSTIN FLEMING <dfleming14@cnm.edu>
2018-10-04 21:06:27 -0600

    Added Assignments.

---
+++ prework-asp/Models/Assignments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Assignments
+    {
+        public string Project { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Issues { get; set; }
+
+    }
+}
