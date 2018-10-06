4745be16d005a7656a2a37e1db35655c439ed27c
Eliot Ostling <it.treugott@gmail.com>
2018-10-06 12:17:41 -0600

    added progress

---
+++ prework-asp/Models/Progress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Progress
+    {
+        public DateTime StartedOn { get; set; }
+        public int PreworkTask1 { get; set; }
+        public int PreworkTask2 { get; set; }
+        public int PreworkTask3 { get; set; }
+        public int PreworkTask4 { get; set; }
+        public int PreworkTask5 { get; set; }
+        public int CompletedAmount { get; set; }
+        public Boolean Alert { get; set; }
+        public Boolean Finished { get; set; }
+    }
+}
