adc82d34ea2627babac465513cd402c9a3752ace
Eliot Ostling <it.treugott@gmail.com>
2018-10-06 19:38:53 -0600

    Added admin

---
+++ prework-asp/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Admin 
+    {
+        public int AdminID { get; set; }
+        public Boolean isAdmin { get; set; }
+    }
+}
