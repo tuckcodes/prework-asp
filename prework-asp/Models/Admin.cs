107b35b31f86975c2e41b8acb2c36cfa4f5be578
DUSTIN FLEMING <dfleming14@cnm.edu>
2018-10-10 20:26:52 -0600

    Removed IStudent,and ITeacher. Added IPerson and made changes to Admin class.

--- prework-asp/Models/Admin.cs
+++ prework-asp/Models/Admin.cs
@@ -5,9 +5,50 @@ using System.Threading.Tasks;
 
 namespace prework_asp.Models
 {
-    public class Admin 
+    public class Admin : IPerson
     {
         public int AdminID { get; set; }
+
         public Boolean isAdmin { get; set; }
+
+        string IPerson.GetFirstName()
+        {
+            return "First Name";
+        }
+
+        string IPerson.GetLastName()
+        {
+            return "Last Name";
+        }
+
+        string IPerson.GetAddress()
+        {
+            return "Address";
+        }
+
+        string IPerson.GetCity()
+        {
+            return "City";
+        }
+
+        string IPerson.GetState()
+        {
+            return "State";
+        }
+
+        string IPerson.GetPhoneNumber()
+        {
+            return "Phone Number";
+        }
+
+        string IPerson.GetEmailAddress()
+        {
+            return "Email Address";
+        }
+
+        DateTime IPerson.GetDateOfBirth()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ prework-asp/Models/IPerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public interface IPerson
+    {
+        [Display(Name = "First Name")]
+        string GetFirstName();
+
+        [Display(Name = "Last Name")]
+        string GetLastName();
+
+        string GetAddress();
+
+        string GetCity();
+
+        string GetState();
+
+        [Display(Name = "Phone Number")]
+        string GetPhoneNumber();
+
+        string GetEmailAddress();
+
+        [Display(Name = "Date of birth")]
+        DateTime GetDateOfBirth();
+
+    }
+}
