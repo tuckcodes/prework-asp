4e49a8e4d349db8992eb9d3c7840d802dd2f43bc
DUSTIN FLEMING <dfleming14@cnm.edu>
2018-10-11 19:27:46 -0600

    Added student and teacher classes.

---
+++ prework-asp/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Student : IPerson
+    {
+
+        public int StudentID { get; set; }
+
+        public int ProgressID { get; set; }
+
+        public string Cohort { get; set; }
+
+        public string GetAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDateOfBirth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEmailAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFirstName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPhoneNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ prework-asp/Models/Teacher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prework_asp.Models
+{
+    public class Teacher : IPerson
+    {
+
+        public int TeacherID { get; set; }
+
+        public Boolean isTeacher { get; set; }
+
+        public string GetAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDateOfBirth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEmailAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFirstName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPhoneNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
