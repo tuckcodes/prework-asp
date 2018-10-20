92ea40d26149638cc18973623009c4253e782927
Eliot Ostling <eostling@cnm.edu>
2018-10-20 11:55:46 -0600

    database migrations butter_panakes

--- prework-asp/Data/Migrations/20181020175328_butter_pancakes.Designer.cs
+++ prework-asp/Data/Migrations/20181020175328_butter_pancakes.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace prework_asp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181020175328_butter_pancakes")]
+    partial class butter_pancakes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
