337d6aca5f65712effcd160cf66a4db0255ce8ef
Eliot Ostling <eostling@cnm.edu>
2018-10-22 18:08:09 -0600

    Added student service basic logic.

--- prework-asp/Interfaces/IStudentService.cs
+++ prework-asp/Interfaces/IStudentService.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using prework_asp.Models;
 
 namespace prework_asp.Interfaces
 {
     interface IStudentService
     {
-       int GetSId();
+       Student GetSId(Student student);
        void GetProg();
        void GetCohorts();
     }
--- prework-asp/Services/StudentService.cs
+++ prework-asp/Services/StudentService.cs
@@ -3,24 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using prework_asp.Interfaces;
+using prework_asp.Data.Migrations;
+using prework_asp.Models;
 
 namespace prework_asp.Services
 {
     public class StudentService : IStudentService
     {
+        private readonly ApplicationDbContext _context;
+
+        public StudentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void GetCohorts()
         {
-            throw new NotImplementedException();
+            var isCohort = _context.Cohort.SingleOrDefault();
         }
 
         public void GetProg()
         {
-            throw new NotImplementedException();
+            //TODO: define when needed
         }
 
-        public int GetSId()
+        public Student GetSId(Student sUser)
         {
-            throw new NotImplementedException();
+            var result = _context.Student.Add(sUser);
+            _context.SaveChanges();
+            return result;
         }
     }
 
